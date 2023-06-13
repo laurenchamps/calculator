915bef365f683f67a18f41c5a27ddbc558df1eb6
Lauren Champs <laurenkchamps@gmail.com>
2023-06-13 17:11:00 +0930

    Add function to store first value and operator

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 // Get elements
 const numBtns = document.querySelectorAll('.num');
 const operatorBtns = document.querySelectorAll('.operator');
+const keypad = document.querySelector('.keypad');
 
-let num1;
-let num2;
-let operator;
+// let num1;
+// let num2;
+// let operator;
 
 // Complete mathematical operation
 function operate(num1, operator, num2) {
@@ -20,30 +21,95 @@ function operate(num1, operator, num2) {
     }
 }
 
-// Display numbers on screen
-let displayValue = function(e) {
-    const display = document.querySelector('#display');
-    if (display.innerText.length >= 7) {
-        return;
-    }
-    display.innerText += e.target.innerText;
+const display = document.querySelector('#display');
 
-    console.log(`Length: ${display.innerText.length}`);
-    let test = storeNum(display.innerText);
-    console.log(test);
+// Display numbers on screen
+function displayValue(digit) {
+    
+    if (display.innerText.length >= 7) {
+        return Number(display.innerText);
+    }
+    display.innerText += digit;
+    let value = Number(display.innerText);
+    return value;
 };
 
-// Store number
-function storeNum(num) {
-    let num1 = num;
+// // Perform operation
+// function performOperation(e) {
+//     let operator = e.target.innerText;
+//     console.log(operator);
 
-    return Number(num1);
+//     let result = getNumber();
+//     console.log(`Result: ${result}`);
+// }
+
+
+let num1 = null;
+let num2 = 0;
+let operator = null;
+let tempValue = 0;
+let numberStarted = false;
+
+function setMode(e) {
+    
+    if (e.target.classList.contains("num")) {
+        if (!numberStarted) {
+            clearDisplay();
+        }
+        // Toggle numberStarted to true
+        isNumberStarted(true);
+        // Display number entered
+        tempValue = displayValue(e.target.innerText);
+    }
+    
+    if (e.target.classList.contains("operator")) {
+        // Toggle numberStarted to false
+        isNumberStarted(false);
+        // Store operator
+        operator = e.target.innerText;
+        setOperator(operator);
+
+        if (num1 === null) {
+            let numTest = setFirstValue(tempValue);
+            clearTempValue();
+            console.log(numTest);
+            console.log({num1})
+        } else {
+            let secondNumTest = setSecondValue(tempValue);
+            console.log(secondNumTest);
+        }
+
+    }
 }
 
-// console.log(num1);
+function setFirstValue(newValue) {
+    num1 = Number(newValue);
+    return num1;
+}
 
-// Testing
-// console.log(displayValue());
+function setSecondValue(newValue) {
+    num2 = Number(newValue);
+    return num2;
+}
+
+function setOperator(newOperator) {
+    operator = (newOperator);
+    return operator;
+}
+
+function clearTempValue() {
+    tempValue = null;
+}
+
+function isNumberStarted(boolean) {
+   numberStarted = boolean;
+   return numberStarted;
+}
+
+function clearDisplay() {
+    display.innerText = '';
+}
+
 
 // Event listeners
-numBtns.forEach(btn => btn.addEventListener('click', displayValue));
+keypad.addEventListener('click', setMode);
