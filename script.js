f7e8924ba23db4e58633eddbb37d2e6304dac33d
Lauren Champs <laurenkchamps@gmail.com>
2023-06-13 09:50:11 +0930

    Store number entered and limit digits to 7

--- script.js
+++ script.js
@@ -23,8 +23,27 @@ function operate(num1, operator, num2) {
 // Display numbers on screen
 let displayValue = function(e) {
     const display = document.querySelector('#display');
+    if (display.innerText.length >= 7) {
+        return;
+    }
     display.innerText += e.target.innerText;
+
+    console.log(`Length: ${display.innerText.length}`);
+    let test = storeNum(display.innerText);
+    console.log(test);
 };
+
+// Store number
+function storeNum(num) {
+    let num1 = num;
+
+    return Number(num1);
+}
+
+// console.log(num1);
+
+// Testing
+// console.log(displayValue());
 
 // Event listeners
 numBtns.forEach(btn => btn.addEventListener('click', displayValue));
